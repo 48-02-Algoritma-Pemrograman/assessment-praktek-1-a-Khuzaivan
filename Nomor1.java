271ccc26f15ecbfee1a57b96cd5bf97748248242
Khuzaivan <khuzaivan24@gmail.com>
2024-10-24 12:40:54 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,30 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Nomor1 {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int biayaLemburNormal = 10000;
+        int biayaLemburTambahan = 15000;
+        int jamLemburNormal = 16;
+        int sisaJamLembur, jamLembur;
+        double honorLembur=0;
+        
+        
+        System.out.print("Total jam lembur: ");
+        jamLembur = scanner.nextInt();
+        
+        if (jamLembur <= jamLemburNormal) {
+            honorLembur = jamLembur * biayaLemburNormal;
+        } else {
+            honorLembur = jamLemburNormal * biayaLemburNormal;
+            sisaJamLembur = jamLembur - jamLemburNormal;
+            honorLembur += sisaJamLembur * biayaLemburTambahan;
+        }
+        
+        System.out.println("Honor lembur: " + honorLembur);
+        
+        scanner.close();
     }
 }
